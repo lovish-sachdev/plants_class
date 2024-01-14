6cb025a31f53b48bad2408e25194c10ddbe61c9e
lovish-sachdev <137512011+lovish-sachdev@users.noreply.github.com>
2024-01-14 12:44:26 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,39 @@
 # import streamlit as st
-# import time
-# import os,json,io
-# import pandas as pd
-# from pydrive.drive import GoogleDrive
-# from pydrive.auth import GoogleAuth
+import time
+import os,json,io
+import pandas as pd
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
 # from new_app import logic
-# def main():
+def main():
 
-#     file_path=os.path.abspath(__file__)
-#     file_dir=os.path.dirname(file_path)
-#     mycreds=os.path.join(file_dir,"mycreds.txt")
-#     authenticate(mycreds)
+    file_path=os.path.abspath(__file__)
+    file_dir=os.path.dirname(file_path)
+    mycreds=os.path.join(file_dir,"mycreds.txt")
+    authenticate(mycreds)
 
-# def authenticate(mycreds):
-#     gauth=GoogleAuth()
-#     gauth.LoadCredentialsFile(mycreds)
+def authenticate(mycreds):
+    gauth=GoogleAuth()
+    gauth.LoadCredentialsFile(mycreds)
 
-#     if gauth.credentials is None:
-#         gauth.LocalWebserverAuth()
+    if gauth.credentials is None:
+        gauth.LocalWebserverAuth()
     
-#     elif gauth.access_token_expired:
-#         gauth.Refresh()
-#     else:
-#         gauth.Authorize()
-#     gauth.SaveCredentialsFile(mycreds) 
+    elif gauth.access_token_expired:
+        gauth.Refresh()
+    else:
+        gauth.Authorize()
+    gauth.SaveCredentialsFile(mycreds) 
     
-#     drive=GoogleDrive(gauth)
-#     file_id="1m_fvJXVRWBc038xmLyVXFrQeF7PFCkSk"
-#     file = drive.CreateFile({'id': file_id})
-#     content = file.GetContentString()
-#     df = pd.read_csv(io.StringIO(content))
-#     app(df,drive)
+    drive=GoogleDrive(gauth)
+    file_id="1m_fvJXVRWBc038xmLyVXFrQeF7PFCkSk"
+    file = drive.CreateFile({'id': file_id})
+    content = file.GetContentString()
+    df = pd.read_csv(io.StringIO(content))
+    
+    st.dataframe(df,hide_index=True)
+    # app(df,drive)
+
 # def app(df,drive):
 
 #     # streamlit app
@@ -67,13 +70,8 @@ json_path=os.path.join(dir,"index.json")
 # data_path=os.path.join(dir,"data.xlsx")
 # data_save_folder=os.path.join(dir,"flower_leave_fruit_data")
 ## opening and reading index
-def function():
-    with open(json_path,"r") as json_file:
-        index_file=json.load(json_file)
-        index=index_file["index"]
-    st.write(index)
-    index_file["index"]+=1
-    with open(json_path,"w") as json_file:
-        json.dump(index_file,json_file)
-    st.write(index_file["index"])
-st.button(label="run",on_click=lambda:function())
+main()
+with open(json_path,"r") as json_file:
+    index_file=json.load(json_file)
+    index=index_file["index"]
+st.write(index)
