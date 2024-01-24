2f494232b9de2650307bc6f42a3ac4ae49548e53
lovish-sachdev <137512011+lovish-sachdev@users.noreply.github.com>
2024-01-24 12:18:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def main():
         # cap.release()
 
         img_file_buffer = st.camera_input("Take a picture")
-
+        st.write(image_file_buffer)
         if img_file_buffer is not None:
             # To read image file buffer as bytes:
             bytes_data = img_file_buffer.getvalue()
