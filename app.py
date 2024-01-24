61f6301649d76735867af998755ec613ae3e4aa8
lovish-sachdev <137512011+lovish-sachdev@users.noreply.github.com>
2024-01-24 12:31:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,19 +102,19 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 
-img_file_buffer = st.camera_input("Take a picture")
+def capture_image():
+    img_file_buffer = st.camera_input("Take a picture")
 
-if img_file_buffer is not None:
-    # To read image file buffer as a PIL Image:
-    img = Image.open(img_file_buffer)
+    if img_file_buffer is
+ 
+not
+ 
+None:
+        img = Image.open(img_file_buffer)
+        img_array = np.array(img)
 
-    # To convert PIL Image to numpy array:
-    img_array = np.array(img)
+        st.write(type(img_array))
+        st.write(img_array.shape)
 
-    # Check the type of img_array:
-    # Should output: <class 'numpy.ndarray'>
-    st.write(type(img_array))
-
-    # Check the shape of img_array:
-    # Should output shape: (height, width, channels)
-    st.write(img_array.shape)
+if st.button("Capture Image"):
+    capture_image()
